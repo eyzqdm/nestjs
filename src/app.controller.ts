1579e9d3759e9356aac616b978f1c9b0a97999b9
guoqing <guoqing.zyzsz@bytedance.com>
2024-01-04 20:55:46 +0800

    fix uplaod

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,7 @@
 import { Body, Query, Controller, Get, Post, UseInterceptors, UploadedFiles, UploadedFile } from '@nestjs/common';
 import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
 import { AppService } from './app.service';
 const fs = require('fs')
 
@@ -15,23 +17,32 @@ export class AppController {
     // /api/upload 处理大文件上传
     @Post('upload') // Post装饰器 用于将post类型的请求路由到指定处理方法
     // 加载FilesInterceptor拦截器
-    @UseInterceptors(FilesInterceptor('files', 20, {
-        dest: 'uploads'
+    @UseInterceptors(FilesInterceptor('file', 10, {
+
+        storage: diskStorage({
+            destination: './uploads/',
+            filename: (req, file, cb) => {
+                console.log('file.originalname>>>>>>>');
+                return cb(null, file.originalname);
+            },
+        }),
     }))
     // @UploadedFiles()装饰器用于从请求中获取上传的文件
-    uploadFile(@UploadedFiles() files: Array<Express.Multer.File>, @Body() body) {
+    uploadFile(@UploadedFiles() files, @Body() body) {
         console.log('body----', body);
         console.log('files----', files);
         const fileName = body.name.match(/(.+)\-\d+$/)[1];
         const chunkDir = 'uploads/chunks_' + fileName;
+        const filePath = files?.[0]?.path
 
 
 
         if (!fs.existsSync(chunkDir)) {
             fs.mkdirSync(chunkDir);
         }
-        fs.cpSync(files[0].path, chunkDir + '/' + body.name);
-        fs.rmSync(files[0].path);
+        fs.cpSync(filePath, chunkDir + '/' + body.name);
+        fs.rmSync(filePath);
+        return { message: '文件上传成功' };
 
     }
 
@@ -43,16 +54,23 @@ export class AppController {
         const files = fs.readdirSync(chunkDir);
 
         let startPos = 0;
-        files.map(file => {
+        files.map((file, index) => {
             const filePath = chunkDir + '/' + file;
             //创建可读流，分块读取文件内容，不会一次性将整个文件加载到内存中，可以更有效地处理大型文件
             const stream = fs.createReadStream(filePath);
             //读取文件内容，首位相接
             stream.pipe(fs.createWriteStream('uploads/' + name, {
                 start: startPos
-            }))
+            })).on('finish', () => {
+                // 合并完成删除文件
+                if (index === files.length - 1) {
+                    fs.rmdirSync(chunkDir, { recursive: true });
+                }
+            })
             startPos += fs.statSync(filePath).size;
         })
+        //删除文件切片
+        return { message: '文件合并成功' };
     }
 
 
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,19 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TestMiddleware } from './mid.middleware';
+import { MulterModule } from '@nestjs/platform-express'
 
 
 @Module({
-    imports: [],
+    imports: [
+        MulterModule.register({
+            dest: './uploads/',
+        }),
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
+
 export class AppModule implements NestModule {
     configure(consumer: MiddlewareConsumer) {
         // 应用中间件到所有路由
